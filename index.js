081d12411197415d67f4fbebf0a16269dce997bb
Marium-DevoraDev <devoradev18@gmail.com>
2026-09-22 16:42:39 +0500

    jkjj

--- index.js
+++ index.js
@@ -97,3 +97,83 @@
 // }else{
 //     console.log("unmatched")
 // }
+
+
+// function getGrade(score){
+// if(score>=90 && score<=100){
+//     return "A+"
+// }else if(score>=80 && score<=89){
+// return "A"
+// }else if(score>=70 && score<=79){
+//     return "B"
+// }else if(score>=60 && score<=69){
+//     return "C"
+// }else if(score>=33 && score<=59){
+//     return "D"
+// }else if(score>=0 && score<=32){
+//     return "Fail"
+// }else{
+//        return "Invalid Marks"
+// }
+// }
+// console.log(getGrade(97))
+
+//for cleaner version
+
+// function getGrade(score){
+// if(score>=90 && score<=100)  return "A+";
+// if(score>=80 && score<=89) return "A";
+// if(score>=70 && score<=79) return "B";
+// if(score>=60 && score<=69) return "C" ;
+// if(score>=33 && score<=59) return "D";
+// if(score>=0 && score<=32) return "Fail";
+
+//        return "Invalid Marks";
+// }
+
+// console.log(getGrade(67))
+
+//rock, paper, scissor game
+  
+// function rps(player1,player2){
+
+//     if(player1 === "rock" && player2 === "scissor") return "rock wins";
+//     if(player1 === "rock" && player2 === "paper") return "paper wins";
+//     if(player1 === "paper" && player2 === "scissor") return "scissor wins";
+//     if(player1 === "scissor" && player2 === "rock") return "rock wins";
+//     if(player1 === "paper" && player2 === "rock") return "paper wins";
+//     if(player1 === "scissor" && player2 === "paper") return "scissor wins";
+//     return("draw")
+    
+// }
+// console.log(rps("scissor", "paper"))
+
+
+//Loops
+//types of loops
+// for while do-while forin forof foreach
+
+//for-loop
+//for(start,end,change){ // }
+
+//while loop
+
+// start
+//while(end){
+//code
+// change
+// }
+
+// let i=1;
+// while(i<32){
+//     console.log(i);
+//     i++
+// }
+//output--> it wil  print from 1-31
+let j=1;
+while(j<32){
+    j++
+    console.log(j);
+    j++
+}
+//output--> it wil  print from 1-32
